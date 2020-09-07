596dbc703817b40840b1a640b5f3df9e4f59a7ff
Michael Hyldsgaard <michael@hyldsgaard.dk>
2020-09-07 21:20:08 +0000

    Added Terraform to pipeline

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+    backend "azurerm" {
+        resource_group_name = "hyd-terraform-storage"
+        storage_account_name = "hydterraformsa"
+        container_name = "tfstate"
+        key = "terraform.tfstate"
+    }
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "hyd-tf-rg"
     location = "westeurope"
